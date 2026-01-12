3b20e3c44206169c0e5fa57a2dcac27ff1c010ec
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-13 00:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (8.44%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        cnt = {}
-        majority = n // 2 # threshold
-        for i in nums:
-            cnt[i] = cnt.get(i, 0) + 1
-            if cnt[i] > majority:
-                return i
-"""
-Complexity:
-Time: O(n) - Traversal and Hash
-Space: O(n) - Storing of element
-"""
+        nums.sort()
+        return nums[len(nums) // 2]
