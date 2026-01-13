9db780460eb2ce59d94196e8b063438fa12eca1a
DINESH S <140992234+itzdineshx@users.noreply.github.com>
2026-01-14 03:34:58 +0530

    Time: 6 ms (29.81%) | Memory: 20.6 MB (5.33%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,33 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        # Approach 1 - Brute force:
+        # n = len(numbers)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if numbers[i]+numbers[j] == target:
+        #             return(i+1, j+1)
+        #             break
+        """
+        Complexity:
+        Time: O(n^2) - Nested loop
+        Space: O(1)
+        """
+
+        # Approach 2 - Two Pointer
         l, r = 0, len(numbers) -1
         while l < r:
             s = numbers[l] + numbers[r] 
             if s == target: # answer
                 break
-            elif s < target:
+            elif s < target: # move left pointer -> right
                 l += 1
-            else:
+            else: # move right pointer -> left
                 r -= 1
-        return l + 1,r + 1
+        return l + 1,r + 1 # index + 1
+        
+"""
+Complexity:
+Time: O(n) - Traversal
+Space: O(1)
+"""
